8d42ac0d4932c750cf83a383ca0718c345c1ecdc
Michael Pesce <Michael Pesce>
2017-05-02 13:41:48 -0400

    second push, improved JS, buttons render dynamically

--- javascript/site.js
+++ javascript/site.js
@@ -1,12 +1,36 @@
 //set global variable array for gif buttons, array is to be
 //added to with inputs from user
-var gifButtons = ["tayne", "Morty", "Do a Flip!", "Groovy"];
+var gifButtons = ["Tayne", "Ninja Turtles", "Venture Bros",
+ "Pizza", "Groovy", "Kangaroos", "Ballin"];
 
 //create function to render the buttons to the html
 function renderButtons(){
 //empty button box
 $("#btnBox").empty();
+//begin for loop to go through gifButton array and append the 
+//strings inside to buttons in the gif box
  for (var i=0; i<gifButtons.length; i++) {
- 	$(gifButtons).append("#btnBox");
+ 	var newBtn=$("<button>");
+ 	newBtn.addClass("gifButton");
+ 	newBtn.attr("data-name", gifButtons[i]);
+ 	newBtn.text(gifButtons[i]);
+
+ 	$("#btnBox").append(newBtn);
  }
 }
+renderButtons();
+console.log(gifButtons);
+
+
+
+// $("#searchBtn").on("click", function() {
+//       var person = $(this).attr("data-person");
+//       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+//         person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+//       $.ajax({
+//           url: queryURL,
+//           method: "GET"
+//         })
+//         .done(function(response) {
+//           var results = response.data;
